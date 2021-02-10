096a21e872141b34d8fcad0e191c79f974f9457d
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 10:49:04 -0500

    updated class name

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/Application.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/Application.swift
@@ -28,7 +28,7 @@ public struct Application:Codable{
     public var oauthConfiguration:OAuth2Configuration? = nil
     public var passwordlessConfiguration:PasswordlessConfiguration? = nil
     public var registrationConfiguration:RegistrationConfiguration? = nil
-    public var registrationDeletePolicy:ApplicatioNRegistrationDeletePolicy? = nil
+    public var registrationDeletePolicy:ApplicationRegistrationDeletePolicy? = nil
     public var roles:[ApplicationRole]? = nil
     public var samlv2Configuration:SAMLV2Configuration? = nil
     public var state:ObjectState? = nil
@@ -36,7 +36,7 @@ public struct Application:Codable{
     public var verificationEmailTemplateId:UUID? = nil
     public var verifyRegistration:Bool? = nil
     
-    public init(active: Bool? = nil, authenticationTokenConfiguration: AuthenticationTokenConfiguration? = nil, cleanSpeakConfiguration: CleanSpeakConfiguration? = nil, data: [String : JSONObject]? = nil, emailConfiguration: ApplicationEmailConfiguration? = nil, formConfiguration: ApplicationFormConfiguration? = nil, id: UUID? = nil, insertInstant: Date? = nil, jwtConfiguration: JWTConfiguration? = nil, lambdaConfiguration: LambdaConfiguration? = nil, lastUpdateInstant: Date? = nil, loginConfiguration: LoginConfiguration? = nil, name: String? = nil, oauthConfiguration: OAuth2Configuration? = nil, passwordlessConfiguration: PasswordlessConfiguration? = nil, registrationConfiguration: RegistrationConfiguration? = nil, registrationDeletePolicy: ApplicatioNRegistrationDeletePolicy? = nil, roles: [ApplicationRole]? = nil, samlv2Configuration: SAMLV2Configuration? = nil, state: ObjectState? = nil, tenantId: UUID? = nil, verificationEmailTemplateId: UUID? = nil, verifyRegistration: Bool? = nil) {
+    public init(active: Bool? = nil, authenticationTokenConfiguration: AuthenticationTokenConfiguration? = nil, cleanSpeakConfiguration: CleanSpeakConfiguration? = nil, data: [String : JSONObject]? = nil, emailConfiguration: ApplicationEmailConfiguration? = nil, formConfiguration: ApplicationFormConfiguration? = nil, id: UUID? = nil, insertInstant: Date? = nil, jwtConfiguration: JWTConfiguration? = nil, lambdaConfiguration: LambdaConfiguration? = nil, lastUpdateInstant: Date? = nil, loginConfiguration: LoginConfiguration? = nil, name: String? = nil, oauthConfiguration: OAuth2Configuration? = nil, passwordlessConfiguration: PasswordlessConfiguration? = nil, registrationConfiguration: RegistrationConfiguration? = nil, registrationDeletePolicy: ApplicationRegistrationDeletePolicy? = nil, roles: [ApplicationRole]? = nil, samlv2Configuration: SAMLV2Configuration? = nil, state: ObjectState? = nil, tenantId: UUID? = nil, verificationEmailTemplateId: UUID? = nil, verifyRegistration: Bool? = nil) {
         self.active = active
         self.authenticationTokenConfiguration = authenticationTokenConfiguration
         self.cleanSpeakConfiguration = cleanSpeakConfiguration
