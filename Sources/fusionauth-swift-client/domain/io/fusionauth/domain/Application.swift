f2d12e8c18d88fe65c79805c27a92d846a8e3cb7
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 10:45:15 -0500

    Updated Application

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/Application.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/Application.swift
@@ -12,35 +12,54 @@ import Foundation
  */
 
 public struct Application:Codable{
-    public var id:UUID? = nil
     public var active:Bool? = nil
     public var authenticationTokenConfiguration:AuthenticationTokenConfiguration? = nil
     public var cleanSpeakConfiguration:CleanSpeakConfiguration? = nil
-    public var data:JSONObject? = nil
+    public var data:[String:JSONObject]? = nil
+    public var emailConfiguration:ApplicationEmailConfiguration? = nil
+    public var formConfiguration:ApplicationFormConfiguration? = nil
+    public var id:UUID? = nil
+    public var insertInstant:Date? = nil
     public var jwtConfiguration:JWTConfiguration? = nil
+    public var lambdaConfiguration:LambdaConfiguration? = nil
+    public var lastUpdateInstant:Date? = nil
+    public var loginConfiguration:LoginConfiguration? = nil
     public var name:String? = nil
     public var oauthConfiguration:OAuth2Configuration? = nil
+    public var passwordlessConfiguration:PasswordlessConfiguration? = nil
+    public var registrationConfiguration:RegistrationConfiguration? = nil
+    public var registrationDeletePolicy:ApplicatioNRegistrationDeletePolicy? = nil
     public var roles:[ApplicationRole]? = nil
+    public var samlv2Configuration:SAMLV2Configuration? = nil
+    public var state:ObjectState? = nil
     public var tenantId:UUID? = nil
     public var verificationEmailTemplateId:UUID? = nil
     public var verifyRegistration:Bool? = nil
-    public var lambdaConfiguration:LambdaConfiguration? = nil
-
-    public init(id: UUID? = nil, active: Bool? = nil, authenticationTokenConfiguration: AuthenticationTokenConfiguration? = nil, cleanSpeakConfiguration: CleanSpeakConfiguration? = nil, data: JSONObject? = nil, jwtConfiguration: JWTConfiguration? = nil, name: String? = nil, oauthConfiguration: OAuth2Configuration? = nil, roles: [ApplicationRole]? = nil, tenantId: UUID? = nil, verificationEmailTemplateId: UUID? = nil, verifyRegistration: Bool? = nil, lambdaConfiguration: LambdaConfiguration? = nil) {
-        self.id = id
+    
+    public init(active: Bool? = nil, authenticationTokenConfiguration: AuthenticationTokenConfiguration? = nil, cleanSpeakConfiguration: CleanSpeakConfiguration? = nil, data: [String : JSONObject]? = nil, emailConfiguration: ApplicationEmailConfiguration? = nil, formConfiguration: ApplicationFormConfiguration? = nil, id: UUID? = nil, insertInstant: Date? = nil, jwtConfiguration: JWTConfiguration? = nil, lambdaConfiguration: LambdaConfiguration? = nil, lastUpdateInstant: Date? = nil, loginConfiguration: LoginConfiguration? = nil, name: String? = nil, oauthConfiguration: OAuth2Configuration? = nil, passwordlessConfiguration: PasswordlessConfiguration? = nil, registrationConfiguration: RegistrationConfiguration? = nil, registrationDeletePolicy: ApplicatioNRegistrationDeletePolicy? = nil, roles: [ApplicationRole]? = nil, samlv2Configuration: SAMLV2Configuration? = nil, state: ObjectState? = nil, tenantId: UUID? = nil, verificationEmailTemplateId: UUID? = nil, verifyRegistration: Bool? = nil) {
         self.active = active
         self.authenticationTokenConfiguration = authenticationTokenConfiguration
         self.cleanSpeakConfiguration = cleanSpeakConfiguration
         self.data = data
+        self.emailConfiguration = emailConfiguration
+        self.formConfiguration = formConfiguration
+        self.id = id
+        self.insertInstant = insertInstant
         self.jwtConfiguration = jwtConfiguration
+        self.lambdaConfiguration = lambdaConfiguration
+        self.lastUpdateInstant = lastUpdateInstant
+        self.loginConfiguration = loginConfiguration
         self.name = name
         self.oauthConfiguration = oauthConfiguration
+        self.passwordlessConfiguration = passwordlessConfiguration
+        self.registrationConfiguration = registrationConfiguration
+        self.registrationDeletePolicy = registrationDeletePolicy
         self.roles = roles
+        self.samlv2Configuration = samlv2Configuration
+        self.state = state
         self.tenantId = tenantId
         self.verificationEmailTemplateId = verificationEmailTemplateId
         self.verifyRegistration = verifyRegistration
-        self.lambdaConfiguration = lambdaConfiguration
     }
-
 }
 
