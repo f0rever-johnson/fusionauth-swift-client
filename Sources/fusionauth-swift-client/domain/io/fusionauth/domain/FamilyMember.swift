cd471916b814a0314dc1ad12bbf10d9edfc50e4c
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 11:05:43 -0500

    updated FamilyMember

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/FamilyMember.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/FamilyMember.swift
@@ -13,19 +13,19 @@ import Foundation
  */
 
 public class FamilyMember:Codable{
-    
     public var data:[String:JSONObject]? = nil
     public var insertInstant:Date? = nil
+    public var lastUpdateInstant:Date? = nil
     public var owner:Bool? = nil
     public var role:FamilyRole? = nil
     public var userId:UUID? = nil
 
-    public init(data: [String:JSONObject]? = nil, insertInstant: Date? = nil, owner: Bool? = nil, role: FamilyRole? = nil, userId: UUID? = nil) {
+    public init(data: [String : JSONObject]? = nil, insertInstant: Date? = nil, lastUpdateInstant: Date? = nil, owner: Bool? = nil, role: FamilyRole? = nil, userId: UUID? = nil) {
         self.data = data
         self.insertInstant = insertInstant
+        self.lastUpdateInstant = lastUpdateInstant
         self.owner = owner
         self.role = role
         self.userId = userId
     }
-
 }
