ba253fa978320ce9bc736023dc601012fc5e7618
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 11:16:02 -0500

    updated LambdaType

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/LambdaType.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/LambdaType.swift
@@ -10,13 +10,20 @@ import Foundation
 /**
  * The types of lambdas that indicate how they are invoked by FusionAuth.
  *
- * @author Everaldlee Johnson
  */
 
 public enum LambdaType:String, Codable{
     
     case JWTPopulate = "JWTPopulate"
     case OpenIDreconcile = "OpenIDreconcile"
-    case SAMLV2Reconcile = "SAMLV2Reconcile"
-    case SAMLV2Populate = "SAMLV2Populate"
+    case SAMLv2Reconcile = "SAMLV2Reconcile"
+    case SAMLv2Populate = "SAMLV2Populate"
+    case AppleReconcile = "AppleReconfile"
+    case ExternalJWTReconcile = "ExternalJWTReconcile"
+    case FacebookReconcile = "FacebookReconcile"
+    case GoogleReconcile = "Google Reconcile"
+    case HYPRReconcile = "HYPRReconcile"
+    case TwitterReconcile = "TwitterReconcile"
+    case LDAPConnectorReconcile = "LDAPConnectorReconcile"
+    case LinkedInReconcile = "LinkedInReconcile"
 }
