5ed7b15918c787fb7a14a063257b8ca34e37b358
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 11:54:49 -0500

    updated SAMLV2Configuration

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/SAMLv2Configuration.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/SAMLv2Configuration.swift
@@ -10,21 +10,30 @@ import Foundation
 public class SAMLV2Configuration:Enableable, Codable{
     public var enabled: Bool? = nil
     public var audience:String? = nil
+    public var authorizedRedirectURLS:[String]? = nil
     public var callbackURL:String? = nil
     public var debug:Bool? = nil
+    public var defaultVerificationKeyId:UUID? = nil
     public var issuer:String? = nil
     public var keyId:UUID? = nil
     public var logoutURL:String? = nil
+    public var requireSignedRequests:Bool? = nil
     public var xmlSignatureC14nMethod:CanonicalizationMethod? = nil
-
-    public init(enabled: Bool? = nil, audience: String? = nil, callbackURL: String? = nil, debug: Bool? = nil, issuer: String? = nil, keyId: UUID? = nil, logoutURL: String? = nil, xmlSignatureC14nMethod: CanonicalizationMethod? = nil) {
+    public var xmlSignatureLocation:XMLSignatureLocation? = nil
+    
+    public init(enabled: Bool? = nil, audience: String? = nil, authorizedRedirectURLS: [String]? = nil, callbackURL: String? = nil, debug: Bool? = nil, defaultVerificationKeyId: UUID? = nil, issuer: String? = nil, keyId: UUID? = nil, logoutURL: String? = nil, requireSignedRequests: Bool? = nil, xmlSignatureC14nMethod: CanonicalizationMethod? = nil, xmlSignatureLocation:XMLSignatureLocation? = nil) {
         self.enabled = enabled
         self.audience = audience
+        self.authorizedRedirectURLS = authorizedRedirectURLS
         self.callbackURL = callbackURL
         self.debug = debug
+        self.defaultVerificationKeyId = defaultVerificationKeyId
         self.issuer = issuer
         self.keyId = keyId
         self.logoutURL = logoutURL
+        self.requireSignedRequests = requireSignedRequests
         self.xmlSignatureC14nMethod = xmlSignatureC14nMethod
+        self.xmlSignatureLocation = xmlSignatureLocation
     }
+
 }
