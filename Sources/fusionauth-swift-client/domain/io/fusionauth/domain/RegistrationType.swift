95321133535b16477892424cf813fb40f2728db9
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 11:46:58 -0500

    Added RegistrationType

---
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/RegistrationType.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Everaldlee Johnson on 2/10/21.
+//
+
+import Foundation
+
+public enum RegistrationType:String, Codable{
+    case basic = "basic"
+    case advanced = "advanced"
+}
