94132db3387c0b822312ad7ad13fd63736e353b0
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 11:46:49 -0500

    update PasswordValidationRules

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/PasswordValidationRules.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/PasswordValidationRules.swift
@@ -12,20 +12,23 @@ import Foundation
  */
 
 public class PasswordValidationRules:Codable{
+   
     public var maxLength:Int? = nil
     public var minLength:Int? = nil
     public var rememberPreviousPasswords:RememberPreviousPasswords? = nil
     public var requireMixedCase:Bool? = nil
     public var requireNonAlpha:Bool? = nil
     public var requireNumber:Bool? = nil
-
-    public init(maxLength: Int? = nil, minLength: Int? = nil, rememberPreviousPasswords: RememberPreviousPasswords? = nil, requireMixedCase: Bool? = nil, requireNonAlpha: Bool? = nil, requireNumber: Bool? = nil) {
+    public var validateOnLogin:Bool = nil
+    
+    internal init(maxLength: Int? = nil, minLength: Int? = nil, rememberPreviousPasswords: RememberPreviousPasswords? = nil, requireMixedCase: Bool? = nil, requireNonAlpha: Bool? = nil, requireNumber: Bool? = nil, validateOnLogin: Bool = nil) {
         self.maxLength = maxLength
         self.minLength = minLength
         self.rememberPreviousPasswords = rememberPreviousPasswords
         self.requireMixedCase = requireMixedCase
         self.requireNonAlpha = requireNonAlpha
         self.requireNumber = requireNumber
+        self.validateOnLogin = validateOnLogin
     }
 
 }
