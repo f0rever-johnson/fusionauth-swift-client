3cf0bbadaf212ecc6be7de07c8b8278d35b210d3
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 11:53:11 -0500

    Added XMLSignatureLocation

---
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/XMLSignatureLocation.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Everaldlee Johnson on 2/10/21.
+//
+
+import Foundation
+
+
+public enum XMLSignatureLocation:String, Codable{
+    case Assertion:"Assertion"
+    case Response:"Response"
+    
+}
