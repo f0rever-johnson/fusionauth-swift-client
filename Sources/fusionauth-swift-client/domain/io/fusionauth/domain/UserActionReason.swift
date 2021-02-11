391b046f620063f6199b89126cccd9b2eee5a0f4
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 19:52:41 -0500

    Updated UserActionReason

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/UserActionReason.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/UserActionReason.swift
@@ -15,12 +15,16 @@ public class UserActionReason:Codable {
 
     public var code:String? = nil
     public var id:UUID? = nil
+    public var insertInstant:Date? = nil
+    public var lastUpdateInstant:Date? = nil
     public var localizedTexts:LocalizedStrings? = nil
     public var text:String? = nil
 
-    public init(code: String? = nil, id: UUID? = nil, localizedTexts: LocalizedStrings? = nil, text: String? = nil) {
+    public init(code: String? = nil, id: UUID? = nil, insertInstant: Date? = nil, lastUpdateInstant: Date? = nil, localizedTexts: LocalizedStrings? = nil, text: String? = nil) {
         self.code = code
         self.id = id
+        self.insertInstant = insertInstant
+        self.lastUpdateInstant = lastUpdateInstant
         self.localizedTexts = localizedTexts
         self.text = text
     }
