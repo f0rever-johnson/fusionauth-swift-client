86b31106d4e192f938cd8e12965468f6cdc182c2
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-11 14:50:01 -0500

    updated SAMLv2IdentityProvider

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/provider/SAMLv2IdentityProvider.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/provider/SAMLv2IdentityProvider.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 public class SAMLv2IdentityProvider:BaseIdentityProvider{
-    public typealias D = SAMLv2ApplicationConfiguration
     
+    public typealias D = SAMLv2ApplicationConfiguration
     
     public var applicationConfiguration: [UUID : SAMLv2ApplicationConfiguration]? = nil
     public var data: [String : JSONObject]? = nil
@@ -25,10 +25,15 @@ public class SAMLv2IdentityProvider:BaseIdentityProvider{
     public var idpEndpoint:String? = nil
     public var issuer:String? = nil
     public var keyId:UUID? = nil
+    public var postRequest:Bool? = nil
+    public var requestSigningKeyId:UUID? = nil
+    public var signRequest:Bool? = nil
     public var lambdaConfiguration:LambdaConfiguration? = nil
     public var useNameIdForEmail:Bool? = nil
+    public var insertinstant: Date? = nil
+    public var lastUpdateInstant: Date? = nil
 
-    public init(applicationConfiguration: [UUID : SAMLv2ApplicationConfiguration]? = nil, data: [String : JSONObject]? = nil, debug: Bool? = nil, id: UUID? = nil, name: String? = nil, type: IdentityProviderType? = nil, enabled: Bool? = nil, buttonImageURL: String? = nil, buttonText: String? = nil, domains: [String]? = nil, emailClaim: String? = nil, idpEndpoint: String? = nil, issuer: String? = nil, keyId: UUID? = nil, lambdaConfiguration: LambdaConfiguration? = nil, useNameIdForEmail: Bool? = nil) {
+    public init(applicationConfiguration: [UUID : SAMLv2ApplicationConfiguration]? = nil, data: [String : JSONObject]? = nil, debug: Bool? = nil, id: UUID? = nil, name: String? = nil, type: IdentityProviderType? = nil, enabled: Bool? = nil, buttonImageURL: String? = nil, buttonText: String? = nil, domains: [String]? = nil, emailClaim: String? = nil, idpEndpoint: String? = nil, issuer: String? = nil, keyId: UUID? = nil, postRequest: Bool? = nil, requestSigningKeyId: UUID? = nil, signRequest: Bool? = nil, lambdaConfiguration: LambdaConfiguration? = nil, useNameIdForEmail: Bool? = nil, insertinstant: Date? = nil, lastUpdateInstant: Date? = nil) {
         self.applicationConfiguration = applicationConfiguration
         self.data = data
         self.debug = debug
@@ -43,9 +48,13 @@ public class SAMLv2IdentityProvider:BaseIdentityProvider{
         self.idpEndpoint = idpEndpoint
         self.issuer = issuer
         self.keyId = keyId
+        self.postRequest = postRequest
+        self.requestSigningKeyId = requestSigningKeyId
+        self.signRequest = signRequest
         self.lambdaConfiguration = lambdaConfiguration
         self.useNameIdForEmail = useNameIdForEmail
+        self.insertinstant = insertinstant
+        self.lastUpdateInstant = lastUpdateInstant
     }
-
 
 }
