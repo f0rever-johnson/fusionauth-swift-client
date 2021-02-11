2bd24dc5f77d64b6aa44a60f4b042b5f09fc3b63
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-11 15:24:51 -0500

    update UserPasswordEvent  & JWTRefreshEvent

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/event/JWTRefreshEvent.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/event/JWTRefreshEvent.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+/**
+ * Models the JWT Refresh Event. This event will be fired when a JWT is "refreshed" (generated) using a Refresh Token.
+ *
+ */
+
+public class JWTRefreshEvent:BaseEvent, Codable{
+    public var createInstant: Date? = nil
+    public var id: UUID? = nil
+    public var tenantId: UUID? = nil
+    public var applicationId:UUID? = nil
+    public var original:String? = nil
+    public var refreshToken:String? = nil
+    public var token:String? = nil
+    public var userId:UUID?
+    
+    public init(createInstant: Date? = nil, id: UUID? = nil, tenantId: UUID? = nil, applicationId: UUID? = nil, original: String? = nil, refreshToken: String? = nil, token: String? = nil, userId: UUID? = nil) {
+        self.createInstant = createInstant
+        self.id = id
+        self.tenantId = tenantId
+        self.applicationId = applicationId
+        self.original = original
+        self.refreshToken = refreshToken
+        self.token = token
+        self.userId = userId
+    }
+    
+    
+}
--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/event/UserPasswordBreachEvent.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/event/UserPasswordBreachEvent.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+public class UserPasswordEvent:BaseEvent{
+    public var createInstant: Date? = nil
+    public var id: UUID? = nil
+    public var tenantId: UUID?
+    public var user:User? = nil
+    
+    public init(createInstant: Date? = nil, id: UUID? = nil, tenantId: UUID? = nil, user: User? = nil) {
+        self.createInstant = createInstant
+        self.id = id
+        self.tenantId = tenantId
+        self.user = user
+    }
+}
