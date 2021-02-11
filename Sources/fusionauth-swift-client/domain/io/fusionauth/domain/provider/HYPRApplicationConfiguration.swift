995b5527a48a192dbb8e27783968085bb782221d
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-11 14:22:56 -0500

    Updated HYPRApplicationConfiguration

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/provider/HYPRApplicationConfiguration.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/provider/HYPRApplicationConfiguration.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+
+public class HYPRApplicationConfiguration:BaseIdentityProviderApplicationConfiguration{
+    public var createRegistration: Bool? = nil
+    public var data: [String : JSONObject]? = nil
+    public var enabled: Bool? = nil
+    public var relyingPartyApplicationId:String? = nil
+    public var relyingPartyURL:String? = nil
+    
+    public init(createRegistration: Bool? = nil, data: [String : JSONObject]? = nil, enabled: Bool? = nil, relyingPartyApplicationId: String? = nil, relyingPartyURL: String? = nil) {
+        self.createRegistration = createRegistration
+        self.data = data
+        self.enabled = enabled
+        self.relyingPartyApplicationId = relyingPartyApplicationId
+        self.relyingPartyURL = relyingPartyURL
+    }
+    
+    
+    
+    
+    
+}
+
