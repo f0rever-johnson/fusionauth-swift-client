f019f110931dcfc6a4d6f91f7ffbfe425b3534d2
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 19:46:06 -0500

    Updated Tenant

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/Tenant.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/Tenant.swift
@@ -13,43 +13,55 @@ import Foundation
 
 public struct Tenant:Codable{
     public var configured:Bool? = nil
+    public var connectorPolicies:[ConnectorPolicy]? = nil
     public var data: [String:JSONObject]? = nil
     public var emailConfiguration:EmailConfiguration? = nil
     public var eventConfiguration:EventConfiguration? = nil
     public var externalIdentifierConfiguration:ExternalIdentifierConfiguration? = nil
     public var failedAuthenticationConfiguration:FailedAuthenticationConfiguration? = nil
     public var familyConfiguration:FamilyConfiguration? = nil
+    public var formConfiguration:TenantFormConfiguration? = nil
     public var httpSessionMaxInactiveInterval:Int? = nil
     public var id:UUID? = nil
+    public var insertInstant:Date? = nil
     public var issuer:String? = nil
     public var jwtConfiguration:JWTConfiguration? = nil
+    public var lastUpdateInstant:Date? = nil
     public var logoutURL:String? = nil
     public var maximumPasswordAge:MaximumPasswordAge? = nil
     public var minimumPasswordAge:MinimumPasswordAge? = nil
     public var name:String? = nil
     public var passwordEncrytionConfiguration:PasswordEncryptionConfiguration? = nil
     public var passwordValidationRules:PasswordValidationRules? = nil
+    public var state:ObjectState? = nil
     public var themeId:UUID? = nil
-
-    public init(configured: Bool? = nil, data: [String:JSONObject]? = nil, emailConfiguration: EmailConfiguration? = nil, eventConfiguration: EventConfiguration? = nil, externalIdentifierConfiguration: ExternalIdentifierConfiguration? = nil, failedAuthenticationConfiguration: FailedAuthenticationConfiguration? = nil, familyConfiguration: FamilyConfiguration? = nil, httpSessionMaxInactiveInterval: Int? = nil, id: UUID? = nil, issuer: String? = nil, jwtConfiguration: JWTConfiguration? = nil, logoutURL: String? = nil, maximumPasswordAge: MaximumPasswordAge? = nil, minimumPasswordAge: MinimumPasswordAge? = nil, name: String? = nil, passwordEncrytionConfiguration: PasswordEncryptionConfiguration? = nil, passwordValidationRules: PasswordValidationRules? = nil, themeId: UUID? = nil) {
+    public var userDeletePolicy:TenantUserDeletePolicy? = nil
+    
+    public init(configured: Bool? = nil, connectorPolicies: [ConnectorPolicy]? = nil, data: [String : JSONObject]? = nil, emailConfiguration: EmailConfiguration? = nil, eventConfiguration: EventConfiguration? = nil, externalIdentifierConfiguration: ExternalIdentifierConfiguration? = nil, failedAuthenticationConfiguration: FailedAuthenticationConfiguration? = nil, familyConfiguration: FamilyConfiguration? = nil, formConfiguration: TenantFormConfiguration? = nil, httpSessionMaxInactiveInterval: Int? = nil, id: UUID? = nil, insertInstant: Date? = nil, issuer: String? = nil, jwtConfiguration: JWTConfiguration? = nil, lastUpdateInstant: Date? = nil, logoutURL: String? = nil, maximumPasswordAge: MaximumPasswordAge? = nil, minimumPasswordAge: MinimumPasswordAge? = nil, name: String? = nil, passwordEncrytionConfiguration: PasswordEncryptionConfiguration? = nil, passwordValidationRules: PasswordValidationRules? = nil, state: ObjectState? = nil, themeId: UUID? = nil, userDeletePolicy: TenantUserDeletePolicy? = nil) {
         self.configured = configured
+        self.connectorPolicies = connectorPolicies
         self.data = data
         self.emailConfiguration = emailConfiguration
         self.eventConfiguration = eventConfiguration
         self.externalIdentifierConfiguration = externalIdentifierConfiguration
         self.failedAuthenticationConfiguration = failedAuthenticationConfiguration
         self.familyConfiguration = familyConfiguration
+        self.formConfiguration = formConfiguration
         self.httpSessionMaxInactiveInterval = httpSessionMaxInactiveInterval
         self.id = id
+        self.insertInstant = insertInstant
         self.issuer = issuer
         self.jwtConfiguration = jwtConfiguration
+        self.lastUpdateInstant = lastUpdateInstant
         self.logoutURL = logoutURL
         self.maximumPasswordAge = maximumPasswordAge
         self.minimumPasswordAge = minimumPasswordAge
         self.name = name
         self.passwordEncrytionConfiguration = passwordEncrytionConfiguration
         self.passwordValidationRules = passwordValidationRules
+        self.state = state
         self.themeId = themeId
+        self.userDeletePolicy = userDeletePolicy
     }
 }
 
