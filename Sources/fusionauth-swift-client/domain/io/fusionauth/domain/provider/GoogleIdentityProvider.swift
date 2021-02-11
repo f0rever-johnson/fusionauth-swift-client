45fafb29b27b81ed0c29ad738846072df20cabbe
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-11 14:20:59 -0500

    updated GoogleIdentityProvider

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/provider/GoogleIdentityProvider.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/provider/GoogleIdentityProvider.swift
@@ -12,8 +12,12 @@ import Foundation
  */
 
 public struct GoogleIdentityProvider:BaseIdentityProvider{
+
     public typealias D = GoogleApplicationConfiguration
     
+    public var insertinstant: Date?
+    public var lambdaConfiguration: LambdaConfiguration?
+    public var lastUpdateInstant: Date?
     public var applicationConfiguration: [UUID : GoogleApplicationConfiguration]? = nil
     public var data: [String : JSONObject]? = nil
     public var debug: Bool? = nil
@@ -26,7 +30,10 @@ public struct GoogleIdentityProvider:BaseIdentityProvider{
     public var clientSecret:String? = nil
     public var scope:String? = nil
 
-    public init(applicationConfiguration: [UUID : GoogleApplicationConfiguration]? = nil, data: [String : JSONObject]? = nil, debug: Bool? = nil, id: UUID? = nil, name: String? = nil, type: IdentityProviderType? = nil, enabled: Bool? = nil, buttonText: String? = nil, clientId: String? = nil, clientSecret: String? = nil, scope: String? = nil) {
+    public init(insertinstant: Date? = nil, lambdaConfiguration: LambdaConfiguration? = nil, lastUpdateInstant: Date? = nil, applicationConfiguration: [UUID : GoogleApplicationConfiguration]? = nil, data: [String : JSONObject]? = nil, debug: Bool? = nil, id: UUID? = nil, name: String? = nil, type: IdentityProviderType? = nil, enabled: Bool? = nil, buttonText: String? = nil, clientId: String? = nil, clientSecret: String? = nil, scope: String? = nil) {
+        self.insertinstant = insertinstant
+        self.lambdaConfiguration = lambdaConfiguration
+        self.lastUpdateInstant = lastUpdateInstant
         self.applicationConfiguration = applicationConfiguration
         self.data = data
         self.debug = debug
