5aec32a5b4cea2e7386cdd77f59f548887c32d06
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-11 12:25:29 -0500

    Updated JWT

--- Sources/fusionauth-swift-client/domain/io/fusionauth/jwt/domain/JWT.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/jwt/domain/JWT.swift
@@ -15,6 +15,17 @@ public class JWT:Codable{
     public var iss:String? = nil
     public var jti:String? = nil
     public var nbf:Date? = nil
+    
+    subscript(claim:String) -> JSONObject?{
+        get{
+            otherClaims[claim]
+        }
+        set(value){
+            otherClaims[claim] = value
+        }
+    }
+    
+    private var otherClaims:[String:JSONObject] = [String:JSONObject]()
     public var sub:String? = nil
 
 }
