2939fb8aab2d1a232acf834192550155e23338b1
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 19:38:19 -0500

    Updated Secure Identity

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/SecureIdentity.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/SecureIdentity.swift
@@ -7,14 +7,23 @@
 
 import Foundation
 
-
 public protocol SecureIdentity:Codable{
-    var encryptionScheme:String? { get }
-    var factor:Int? { get }
-    var id:UUID? { get }
-    var password:String? { get }
-    var passwordChangeRequired:Bool? { get }
-    var passwordLastUpdateInstant:Date? { get }
-    var salt:String? { get }
-    var verified:Bool? { get }
+    var breachedPasswordLastCheckedInstant:Date? { get set }
+    var breachedPasswordStatus:BreachedPasswordStatus? { get set }
+    var connectorId:UUID? { get set }
+    var encryptionScheme:String? { get set }
+    var factor:Int? { get set }
+    var id:UUID? { get set }
+    var lastLoginInstant:Date? { get set }
+    var password:String? { get set }
+    var passwordChangeReason:ChangePasswordReason? { get set }
+    var passwordChangedRequired:Bool? { get set }
+    var passwordLastUpdateInstant:Date? { get set }
+    var salt:String? { get set }
+    var twoFactorDelivery:TwoFactorDelivery? { get set }
+    var twoFactorEnabled:Bool? { get set }
+    var twoFactorSecret:String? { get set }
+    var username:String? { get set }
+    var usernameStatus:ContentStatus? { get set }
+    var verified:Bool? { get set }
 }
