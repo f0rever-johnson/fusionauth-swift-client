d48c78db7fbb5e1a8d86c83782d718fcc26f15a6
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-11 14:27:49 -0500

    updated IdentityProviderOAuth2Configuration

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/provider/IdentityProviderOAuth2Configuration.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/provider/IdentityProviderOAuth2Configuration.swift
@@ -12,23 +12,29 @@ import Foundation
  */
 
 public struct IdentityProviderOAuth2Configuration:Codable {
+    
     public var authorizationEndpoint:String? = nil
+    public var clientAuthenticationMethod:ClientAuthenticationMethod? = nil
     public var clientId:String? = nil
     public var clientSecret:String? = nil
+    public var emailClaim:String? = nil
     public var issuer:String? = nil
     public var scope:String? = nil
     public var tokenEndpoint:String? = nil
     public var userinfoEndpoint:String? = nil
 
-    public init(authorizationEndpoint: String? = nil, clientId: String? = nil, clientSecret: String? = nil, issuer: String? = nil, scope: String? = nil, tokenEndpoint: String? = nil, userinfoEndpoint: String? = nil) {
+    public init(authorizationEndpoint: String? = nil, clientAuthenticationMethod: ClientAuthenticationMethod? = nil, clientId: String? = nil, clientSecret: String? = nil, emailClaim: String? = nil, issuer: String? = nil, scope: String? = nil, tokenEndpoint: String? = nil, userinfoEndpoint: String? = nil) {
         self.authorizationEndpoint = authorizationEndpoint
+        self.clientAuthenticationMethod = clientAuthenticationMethod
         self.clientId = clientId
         self.clientSecret = clientSecret
+        self.emailClaim = emailClaim
         self.issuer = issuer
         self.scope = scope
         self.tokenEndpoint = tokenEndpoint
         self.userinfoEndpoint = userinfoEndpoint
     }
+  
 
     private enum CodingKeys:String, CodingKey{
         case authorizationEndpoint = "authorizationEndpoint"
