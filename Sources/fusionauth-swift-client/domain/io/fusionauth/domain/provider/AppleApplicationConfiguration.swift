2b5d1b19dc64e5150dfc74374fa5e05f15e9e288
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-11 12:30:28 -0500

    Updated AppleApplicationConfiguration

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/provider/AppleApplicationConfiguration.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/provider/AppleApplicationConfiguration.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+public class AppleApplicationConfiguration:BaseIdentityProviderApplicationConfiguration, Codable{
+    public var createRegistration: Bool? = nil
+    public var data: [String : JSONObject]? = nil
+    public var enabled: Bool? = nil
+    public var buttonText:String? = nil
+    public var keyId:UUID? = nil
+    public var scope:String? = nil
+    public var servicesId:String? = nil
+    public var teamId:String? = nil
+    
+    public init(createRegistration: Bool? = nil, data: [String : JSONObject]? = nil, enabled: Bool? = nil, buttonText: String? = nil, keyId: UUID? = nil, scope: String? = nil, servicesId: String? = nil, teamId: String? = nil) {
+        self.createRegistration = createRegistration
+        self.data = data
+        self.enabled = enabled
+        self.buttonText = buttonText
+        self.keyId = keyId
+        self.scope = scope
+        self.servicesId = servicesId
+        self.teamId = teamId
+    }
+}
