c9be1d1d96878e10cd86bc91cb12f1e083457223
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-11 12:34:41 -0500

    updated AppleIdentityProvider

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/provider/AppleIdentityProvider.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/provider/AppleIdentityProvider.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+public class AppleIdentityProvider:BaseIdentityProvider, Codable{
+
+    public typealias D = AppleApplicationConfiguration
+    public var applicationConfiguration: [UUID : AppleApplicationConfiguration]? = nil
+    public var data: [String : JSONObject]?  = nil
+    public var debug: Bool?  = nil
+    public var id: UUID? = nil
+    public var name: String? = nil
+    public var type: IdentityProviderType? = nil
+    public var enabled: Bool? = nil
+    public var buttonText:String? = nil
+    public var keyId:UUID? = nil
+    public var scope:String? = nil
+    public var servicesId:String? = nil
+    public var teamId:String? = nil
+    
+    public init(applicationConfiguration: [UUID : AppleApplicationConfiguration]? = nil, data: [String : JSONObject]? = nil, debug: Bool? = nil, id: UUID? = nil, name: String? = nil, type: IdentityProviderType? = nil, enabled: Bool? = nil, buttonText: String? = nil, keyId: UUID? = nil, scope: String? = nil, servicesId: String? = nil, teamId: String? = nil) {
+        self.applicationConfiguration = applicationConfiguration
+        self.data = data
+        self.debug = debug
+        self.id = id
+        self.name = name
+        self.type = type
+        self.enabled = enabled
+        self.buttonText = buttonText
+        self.keyId = keyId
+        self.scope = scope
+        self.servicesId = servicesId
+        self.teamId = teamId
+    }
+    
+}
