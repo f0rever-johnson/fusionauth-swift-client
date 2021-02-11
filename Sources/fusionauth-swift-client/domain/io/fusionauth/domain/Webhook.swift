c4f865da5033ace764961d8067360691943c6773
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 20:00:21 -0500

    Updated Webhook

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/Webhook.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/Webhook.swift
@@ -13,32 +13,39 @@ import Foundation
  */
 
 public struct Webhook:Codable {
+    
     public var applicationIds:[UUID]? = nil
     public var connectTimeout:Int? = nil
     public var data:[String:JSONObject]? = nil
     public var description:String? = nil
+    public var eventsEnabled:[EventType:Bool]? = nil
     public var global:Bool? = nil
     public var headers:HTTPHeaders? = nil
     public var httpAuthenticationPassword:String? = nil
     public var httpAuthenticationUsername:String? = nil
     public var id:UUID? = nil
+    public var insertInstant:Date? = nil
+    public var lastUpdateInstant:Date? = nil
     public var readTimeout:Int? = nil
     public var sslCertificate:String? = nil
     public var url:String? = nil
-
-
-    public init(applicationIds: [UUID]? = nil, connectTimeout: Int? = nil, data: [String:JSONObject]? = nil, description: String? = nil, global: Bool? = nil, headers: HTTPHeaders? = nil, httpAuthenticationPassword: String? = nil, httpAuthenticationUsername: String? = nil, id: UUID? = nil, readTimeout: Int? = nil, sslCertificate: String? = nil, url: String? = nil) {
+    
+    public init(applicationIds: [UUID]? = nil, connectTimeout: Int? = nil, data: [String : JSONObject]? = nil, description: String? = nil, eventsEnabled: [EventType : Bool]? = nil, global: Bool? = nil, headers: HTTPHeaders? = nil, httpAuthenticationPassword: String? = nil, httpAuthenticationUsername: String? = nil, id: UUID? = nil, insertInstant: Date? = nil, lastUpdateInstant: Date? = nil, readTimeout: Int? = nil, sslCertificate: String? = nil, url: String? = nil) {
         self.applicationIds = applicationIds
         self.connectTimeout = connectTimeout
         self.data = data
         self.description = description
+        self.eventsEnabled = eventsEnabled
         self.global = global
         self.headers = headers
         self.httpAuthenticationPassword = httpAuthenticationPassword
         self.httpAuthenticationUsername = httpAuthenticationUsername
         self.id = id
+        self.insertInstant = insertInstant
+        self.lastUpdateInstant = lastUpdateInstant
         self.readTimeout = readTimeout
         self.sslCertificate = sslCertificate
         self.url = url
     }
+
 }
