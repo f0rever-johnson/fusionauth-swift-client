812565213b04fa15dd5fc0846d8bb6d3da6037e0
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-10 19:50:33 -0500

    Updated UserActionLog

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/UserActionLog.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/UserActionLog.swift
@@ -12,7 +12,6 @@ import Foundation
  */
 
 public struct UserActionLog:Codable {
-
     public var actioneeUserId:UUID? = nil
     public var actionerUserId:UUID? = nil
     public var applicationIds:[UUID]? = nil
@@ -23,6 +22,7 @@ public struct UserActionLog:Codable {
     public var expiry:Date? = nil
     public var history:LogHistory? = nil
     public var id:UUID? = nil
+    public var insertInstant:Date? = nil
     public var localizedName:String? = nil
     public var localizedOption:String? = nil
     public var localizedReason:String? = nil
@@ -32,8 +32,8 @@ public struct UserActionLog:Codable {
     public var reason:String? = nil
     public var reasonCode:String? = nil
     public var userActionId:UUID? = nil
-
-    public init(actioneeUserId: UUID? = nil, actionerUserId: UUID? = nil, applicationIds: [UUID]? = nil, comment: String? = nil, createInstant: Date? = nil, emailUserOnEnd: Bool? = nil, endEventSent: Bool? = nil, expiry: Date? = nil, history: LogHistory? = nil, id: UUID? = nil, localizedName: String? = nil, localizedOption: String? = nil, localizedReason: String? = nil, name: String? = nil, notifyUserOnEnd: Bool? = nil, option: String? = nil, reason: String? = nil, reasonCode: String? = nil, userActionId: UUID? = nil) {
+    
+    public init(actioneeUserId: UUID? = nil, actionerUserId: UUID? = nil, applicationIds: [UUID]? = nil, comment: String? = nil, createInstant: Date? = nil, emailUserOnEnd: Bool? = nil, endEventSent: Bool? = nil, expiry: Date? = nil, history: LogHistory? = nil, id: UUID? = nil, insertInstant: Date? = nil, localizedName: String? = nil, localizedOption: String? = nil, localizedReason: String? = nil, name: String? = nil, notifyUserOnEnd: Bool? = nil, option: String? = nil, reason: String? = nil, reasonCode: String? = nil, userActionId: UUID? = nil) {
         self.actioneeUserId = actioneeUserId
         self.actionerUserId = actionerUserId
         self.applicationIds = applicationIds
@@ -44,6 +44,7 @@ public struct UserActionLog:Codable {
         self.expiry = expiry
         self.history = history
         self.id = id
+        self.insertInstant = insertInstant
         self.localizedName = localizedName
         self.localizedOption = localizedOption
         self.localizedReason = localizedReason
