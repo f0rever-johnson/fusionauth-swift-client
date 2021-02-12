66ca50de975352679482fd55790761413f56ddce
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-12 13:53:31 -0500

    updated IdentityProviderDetails

--- Sources/fusionauth-swift-client/domain/io/fusionauth/domain/api/identityProvider/IdentityProviderDetails.swift
+++ Sources/fusionauth-swift-client/domain/io/fusionauth/domain/api/identityProvider/IdentityProviderDetails.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+public class IdentityProviderDetails{
+    public var applications:[UUID]? = nil
+    public var id:UUID? = nil
+    public var idpEndpoint:String?
+    public var name:String? = nil
+    public var oauth2:IdentityProviderOAuth2Configuration = nil
+    public var type:IdentityProviderType? = nil
+    
+    public init(applications: [UUID]? = nil, id: UUID? = nil, idpEndpoint: String? = nil, name: String? = nil, oauth2: IdentityProviderOAuth2Configuration = nil, type: IdentityProviderType? = nil) {
+        self.applications = applications
+        self.id = id
+        self.idpEndpoint = idpEndpoint
+        self.name = name
+        self.oauth2 = oauth2
+        self.type = type
+    }
+}
