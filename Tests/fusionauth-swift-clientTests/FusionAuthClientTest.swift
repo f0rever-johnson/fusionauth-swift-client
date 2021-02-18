b872f6e72bee761efd321fdbb881caac25ec721e
Everaldlee Johnson <21150708+f0rever-johnson@users.noreply.github.com>
2021-02-18 14:46:28 -0500

    Added test cases

--- Tests/fusionauth-swift-clientTests/FusionAuthClientTest.swift
+++ Tests/fusionauth-swift-clientTests/FusionAuthClientTest.swift
@@ -12,7 +12,7 @@ import XCTest
 class FusionAuthClientTest: XCTestCase {
     
     let baseUrl:String = "factoryWinventory.ddns.net"
-    let apiKey:String = "a2LMkmZzCODNvRPbIQ8k-dQ3idlxnshffTTINyCzox_BhuDR6psCYmKw"
+    let apiKey:String = "E9XCHRTDFwc0x_JcdcGmi78PEpcl2PdMcl0pines2DB4Df3jQHjG-Sm-"
     let applicationId:UUID = UUID.init(uuidString: "d5b8a58a-4ceb-4a69-b9d9-0dd706774c14")!
     
     let emailAddress:String = "swiftclient@fusionauth.io"
@@ -39,6 +39,7 @@ class FusionAuthClientTest: XCTestCase {
         user = nil
     }
     
+    
     func AssertSuccess<T>(response: ClientResponse<T>){
         let message:String = response.exception == nil ? "No Errors" : response.exception!.localizedDescription
 
@@ -63,33 +64,42 @@ class FusionAuthClientTest: XCTestCase {
         XCTAssertNil(response.successResponse)
     }
 
-    func CreateUser(email:String, username:String, password:String, applicationId:UUID) -> ClientResponse<RegistrationResponse>{
-        let expectation = XCTestExpectation(description: "Create a User")
-        let newUser:User = User(email: emailAddress, password: password, username: username)
-        let registration:UserRegistration = UserRegistration(applicationId: applicationId, username: username)
-        var userRegisterResponse:ClientResponse<RegistrationResponse> = ClientResponse()
-
-        let registrationRequest:RegistrationRequest = RegistrationRequest(registration: registration, sendSetPasswordEmail: false, skipVerification: true, user: newUser)
-
-
-        client?.Register(userId: nil, request: registrationRequest, clientResponse: { (response) in
-            self.AssertSuccess(response: response)
-            userRegisterResponse = response
-            self.user = userRegisterResponse.successResponse?.user
-            expectation.fulfill()
+    func CreateUser(complete:@escaping(Bool) -> ()){
+        client?.RetrieveUserByEmail(email: emailAddress, clientResponse: { (retrieveUserResponse) in
+            if retrieveUserResponse.WasSuccessful{
+                guard let userId:UUID = retrieveUserResponse.successResponse?.user?.id else{return}
+                self.client?.DeleteUser(userId: userId, hardDelete: true, clientResponse: { (deleteUserResponse) in
+                    self.AssertSuccess(response: deleteUserResponse)
+                    CreateUser()
+                })
+            }else{
+                CreateUser()
+            }
         })
         
-        wait(for: [expectation], timeout: 15)
-        return userRegisterResponse
-      
+        func CreateUser(){
+            let newUser:User = User(email: emailAddress, password: password, username: username)
+            let registration:UserRegistration = UserRegistration(applicationId: applicationId, username: username)
+            let registrationRequest:RegistrationRequest = RegistrationRequest(registration: registration, sendSetPasswordEmail: false, skipVerification: true, user: newUser)
+
+            client?.Register(userId: nil, request: registrationRequest, clientResponse: { (registerUserResponse) in
+                self.AssertSuccess(response: registerUserResponse)
+                guard let userName:String = registerUserResponse.successResponse?.user?.username else{return}
+                XCTAssertEqual(newUser.username, userName)
+                self.user = registerUserResponse.successResponse?.user
+                complete(true)
+            })
+        }
     }
     
-    func CreateUserAction(actionName:String, isTemporal:Bool){
-        client?.RetrieveUserActions(clientResponse: { (retrieveUserAction) in
-            self.AssertSuccess(response: retrieveUserAction)
-            if retrieveUserAction.WasSuccessful{
-                guard let userActions:[UserAction] = retrieveUserAction.successResponse?.userActions else{return}
-                if !userActions.isEmpty{
+    func CreateUserAction(actionName:String, isTemporal:Bool, complete:@escaping(Bool) -> ()){
+       
+        client?.RetrieveUserActions(clientResponse: { (retrieveUserActionResponse) in
+            print("retrieveUserActionResponse")
+            dump(retrieveUserActionResponse)
+            self.AssertSuccess(response: retrieveUserActionResponse)
+            if retrieveUserActionResponse.WasSuccessful{
+                if let userActions = retrieveUserActionResponse.successResponse?.userActions{
                     for userAction in userActions{
                         if userAction.name == actionName{
                             guard let userActionId:UUID = userAction.id else{return}
@@ -99,83 +109,171 @@ class FusionAuthClientTest: XCTestCase {
                         }
                     }
                 }
+                CreateUserAction()
             }
+        })
+        
+        func CreateUserAction(){
             let userAction:UserAction = UserAction(name: actionName, temporal: isTemporal)
             let userActionRequest:UserActionRequest = UserActionRequest(userAction: userAction)
             
             self.client?.CreateUserAction(userActionId: nil, request: userActionRequest, clientResponse: { (createUserActionResponse) in
+                dump(createUserActionResponse.WasSuccessful)
                 self.AssertSuccess(response: createUserActionResponse)
                 self.userAction = createUserActionResponse.successResponse?.userAction
+                //expect.fulfill()
+                complete(true)
             })
-            
-          
+        }
+    }
+    
+    func ActionUser(complete:@escaping(Bool) -> ()){
+        let date:Date = Calendar.current.date(byAdding: .day, value: 2, to: Date())!
+        let expiry:Date = (userAction?.temporal)! ? date : Date()
+        let actionData:ActionData = ActionData(actioneeUserId: user?.id, actionerUserId: user?.id, expiry: expiry, userActionId: userAction?.id)
+        
+        let actionRequest:ActionRequest = ActionRequest(action: actionData, broadcast: false)
+           
+        client?.ActionUser(request: actionRequest, clientResponse: { actionUserResponse in
+            self.AssertSuccess(response: actionUserResponse)
+            self.userActionLog = actionUserResponse.successResponse?.action
+            complete(true)
         })
     }
     
-    func ActionUser(){
-        let expiry:Date = (userAction?.temporal)! ? Date().advanced(by: 1) : Date()
-           let actionData:ActionData = ActionData(actioneeUserId: user?.id, actionerUserId: user?.id, expiry: expiry, userActionId: userAction?.id)
-           
-           let actionRequest:ActionRequest = ActionRequest(action: actionData, broadcast: false)
-           
-           client?.ActionUser(request: actionRequest, clientResponse: { actionUserResponse in
-               self.AssertSuccess(response: actionUserResponse)
-               self.userActionLog = actionUserResponse.successResponse?.action
-           })
-    }
-    
-    func Login(){
-        let loginRequest:LoginRequest = LoginRequest(applicationId: application?.id, loginId: user?.email, password: "password")
+    func Login(complete:@escaping(Bool) -> ()){
+        let loginRequest:LoginRequest = LoginRequest(applicationId: application?.id, loginId: user?.email, password: password)
         client?.Login(request: loginRequest, clientResponse: { loginResponse in
             self.AssertSuccess(response: loginResponse)
-            
             self.token = loginResponse.successResponse?.token
             self.user = loginResponse.successResponse?.user
         })
     }
     
     func UpdateApplication(application:Application){
+        let expect:XCTestExpectation = XCTestExpectation()
         let applicationRequest:ApplicationRequest = ApplicationRequest(application: application)
         client?.UpdateApplication(applicationId: applicationId, request: applicationRequest, clientResponse: { updateApplicationResponse in
             self.AssertSuccess(response: updateApplicationResponse)
             self.application = updateApplicationResponse.successResponse?.application
+            expect.fulfill()
         })
+        wait(for: [expect], timeout: 30)
     }
     
-    func CreateApplication(){
-        let raExpect:XCTestExpectation = XCTestExpectation()
+    func CreateApplication(complete:@escaping (Bool) ->() ){
         client?.RetrieveApplication(applicationId: applicationId, clientResponse: { retrieveApplicationResponse in
             if retrieveApplicationResponse.WasSuccessful{
                 self.client?.DeleteApplication(applicationId: self.applicationId, clientResponse: { deleteApplicationResponse in
+                    dump(deleteApplicationResponse)
                     self.AssertSuccess(response: deleteApplicationResponse)
+                    createApplication()
+                    
                 })
-                raExpect.fulfill()
+            }else{
+                createApplication()
             }
         })
-        let caExpect:XCTestExpectation = XCTestExpectation()
-        let application:Application = Application(name: "Swift Client Test")
-        let applicationRequest:ApplicationRequest = ApplicationRequest(application: application)
-        client?.CreateApplication(applicationId: applicationId, request: applicationRequest, clientResponse: { createApplicationResponse in
-            self.AssertSuccess(response: createApplicationResponse)
-            XCTAssertEqual(application.name, createApplicationResponse.successResponse?.application?.name)
-            self.application = createApplicationResponse.successResponse?.application
-            caExpect.fulfill()
-        })
-        wait(for: [caExpect, raExpect], timeout: 10)
+        
+        func createApplication(){
+            let application:Application = Application(name: "Swift Client Test")
+            let applicationRequest:ApplicationRequest = ApplicationRequest(application: application)
+            client?.CreateApplication(applicationId: applicationId, request: applicationRequest, clientResponse: { createApplicationResponse in
+                self.AssertSuccess(response: createApplicationResponse)
+                guard let name = createApplicationResponse.successResponse?.application?.name else{return}
+                XCTAssertEqual(application.name, name)
+                
+                self.application = createApplicationResponse.successResponse?.application
+                complete(true)
+            })
+        }
     }
     
     func testPatchApplication(){
-        CreateApplication()
-        
-        let request:[String:JSONObject] = [:]
-        client?.PatchApplication(applicationID: applicationId, request: request, clientResponse: { (patchApplicationResponse) in
-            self.AssertSuccess(response: patchApplicationResponse)
-            //XCTAssertFalse(patchApplicationResponse.successResponse?.application.pass)
-        })
-        
-        
+        let expect:XCTestExpectation = XCTestExpectation()
+        CreateApplication { complete in
+            let registrationConfiguration:RegistrationConfiguration = RegistrationConfiguration(enabled:true)
+            let passwordlessConfiguration:PasswordlessConfiguration = PasswordlessConfiguration(enabled: false)
+            
+            let application:Application = Application(passwordlessConfiguration: passwordlessConfiguration, registrationConfiguration:registrationConfiguration)
+            let request:ApplicationRequest = ApplicationRequest(application:application)
+            
+            self.client?.PatchApplication(applicationID: self.applicationId, request: request, clientResponse: { (patchApplicationResponse) in
+                self.AssertSuccess(response: patchApplicationResponse)
+                if patchApplicationResponse.WasSuccessful{
+                    self.client?.RetrieveApplication(applicationId: self.applicationId, clientResponse: { (retrieveApplicationResponse) in
+                        self.AssertSuccess(response: retrieveApplicationResponse)
+                        if retrieveApplicationResponse.WasSuccessful{
+                            guard let passwordlessConfig = patchApplicationResponse.successResponse?.application?.passwordlessConfiguration?.enabled else{return}
+                            guard let registrationConfig = patchApplicationResponse.successResponse?.application?.registrationConfiguration?.enabled else{return}
+                            
+                            XCTAssertFalse(passwordlessConfig)
+                            XCTAssertTrue(registrationConfig)
+                        }
+                        
+                        expect.fulfill()
+                    })
+                }
+            })
+        }
+        wait(for: [expect], timeout: 30)
     }
     
+    func testCancelAction(){
+        let expect:XCTestExpectation = XCTestExpectation()
+        CreateApplication { caComplete in
+            self.CreateUserAction(actionName: "SwiftClientUserAction", isTemporal: true) { (cuaComplete) in
+                self.CreateUser { cuComplete in
+                    self.ActionUser { auComplete in
+                        cancelAction()
+                    }
+                }
+            }
+        }
+        
+        func cancelAction(){
+            let action:ActionData = ActionData(actionerUserId: user?.id, comment: "Cancel Action")
+            let request:ActionRequest = ActionRequest(action:action)
+            
+            guard let userActionLogId = userActionLog?.id else {return}
+            client?.CancelAction(actionId: userActionLogId, request: request, clientResponse: { cancelActionResponse in
+                self.AssertSuccess(response: cancelActionResponse)
+                expect.fulfill()
+            })
+        }
+        wait(for: [expect], timeout: 30)
+    }
     
-    
+    func testCreateTenant(){
+        let expect:XCTestExpectation = XCTestExpectation()
+        client?.RetrieveTenants(clientResponse: { retrieveTenantsResponse in
+            self.AssertSuccess(response: retrieveTenantsResponse)
+            let eventConfigurationData:EventConfigurationData = EventConfigurationData(enabled: true)
+            let eventConfiguration:EventConfiguration = EventConfiguration(events: [EventType.UserDelete.rawValue: eventConfigurationData])
+            var tenant = retrieveTenantsResponse.successResponse?.tenants?[2]
+            tenant?.name = "Swift Tenant"
+            tenant?.eventConfiguration = eventConfiguration
+            let tenantRequest:TenantRequest = TenantRequest(tenant:tenant)
+            
+            dump(tenant)
+            
+            self.client?.CreateTenant(tenantId: nil, request: tenantRequest, clientResponse: { createTenantResponse in
+                self.AssertSuccess(response: createTenantResponse)
+                guard let id = createTenantResponse.successResponse?.tenant?.id else{return}
+                self.client?.RetrieveTenant(tenantId: id, clientResponse: { retrieveTenantResponse in
+                    self.AssertSuccess(response: retrieveTenantResponse)
+                    let userDeleteEvent = retrieveTenantResponse.successResponse?.tenant?.eventConfiguration?.events?[EventType.UserDelete.rawValue]
+                    XCTAssertNotNil(userDeleteEvent)
+                    XCTAssertTrue((userDeleteEvent!.enabled)!)
+                    
+                    self.client?.DeleteTenant(tenantId: id, clientResponse: { deleteTenantResponse in
+                        self.AssertSuccess(response: deleteTenantResponse)
+                        expect.fulfill()
+                    })
+                })
+            })
+        })
+        wait(for: [expect], timeout: 30)
+    }
+
 }
